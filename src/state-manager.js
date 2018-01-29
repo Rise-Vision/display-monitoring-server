8402fc5aeafe5a6a179257abe365f942b53ac065
santiagonoguez <santiago@nul-unu.com>
2018-01-29 15:45:27 -0600

    implement display states

--- src/state-manager.js
+++ src/state-manager.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers, default-case */
+
 // Display ids are used as keys, the value is a structure with state and count of times it has been in that status
 let currentDisplayStates = {};
 
@@ -13,8 +15,63 @@ function filterSilentStates(list) {
   currentDisplayStates = filteredDisplayStates;
 }
 
+function updateAsOnline(entry) {
+  switch (entry.state) {
+    case 'ALERTED':
+      Object.assign(entry, {state: 'RECOVERING', count: 1});
+      return null;
+
+    case 'RECOVERING':
+      if (entry.count === 2) {
+        entry.state = 'OK';
+        return "SEND_RECOVERY_EMAIL";
+      }
+
+      entry.count = 2;
+      return null;
+
+    case 'FAILED':
+      entry.state = 'OK';
+  }
+
+  return null;
+}
+
+function updateAsOffline(entry) {
+  switch (entry.state) {
+    case 'OK':
+      Object.assign(entry, {state: 'FAILED', count: 1});
+      return null;
+
+    case 'FAILED':
+      if (entry.count === 2) {
+        entry.state = 'ALERTED';
+        return "SEND_FAILURE_EMAIL";
+      }
+
+      entry.count = 2;
+      return null;
+
+    case 'RECOVERING':
+      entry.state = 'ALERTED';
+  }
+
+  return null;
+}
+
 function updateDisplayStatus(displayId, online) {
-  currentDisplayStates[displayId] = {online};
+  const displayState = currentDisplayStates[displayId];
+
+  if (!displayState) {
+    currentDisplayStates[displayId] = {
+      state: online ? 'OK' : 'FAILED',
+      count: 1
+    };
+
+    return null;
+  }
+
+  return online ? updateAsOnline(displayState) : updateAsOffline(displayState);
 }
 
 // For inspection during testing only.
--- test/unit/state-manager.js
+++ test/unit/state-manager.js
@@ -1,4 +1,5 @@
 /* eslint-env mocha */
+/* eslint-disable max-statements, no-magic-numbers */
 const assert = require("assert");
 
 const stateManager = require("../../src/state-manager.js");
@@ -23,6 +24,29 @@ describe("State Manager - Unit", ()=>{
     assert(states.A123);
     assert(!states.B123);
     assert(!states.C123);
+  });
+
+  it("should switch between OK and FAILED", () => {
+    stateManager.updateDisplayStatus("A123", true);
+
+    {
+      const states = stateManager.getCurrentDisplayStates();
+      assert.deepEqual(states, {A123: {state: 'OK', count: 1}})
+    }
+
+    stateManager.updateDisplayStatus("A123", false);
+
+    {
+      const states = stateManager.getCurrentDisplayStates();
+      assert.deepEqual(states, {A123: {state: 'FAILED', count: 1}})
+    }
+
+    stateManager.updateDisplayStatus("A123", true);
+
+    {
+      const states = stateManager.getCurrentDisplayStates();
+      assert.deepEqual(states, {A123: {state: 'OK', count: 1}})
+    }
   });
 
 });
