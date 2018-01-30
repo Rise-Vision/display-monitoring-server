8c7b4627fc706645958a976a3796e4451788a0ba
santiagonoguez <santiago@nul-unu.com>
2018-01-30 09:17:55 -0600

    call state manager from notifier

--- src/notifier.js
+++ src/notifier.js
@@ -2,8 +2,28 @@ const stateManager = require("./state-manager");
 
 function updateDisplayStatusList(list) {
   stateManager.filterSilentStates(list);
+
+  list.forEach(({displayId, online, addresses}) => {
+    const action = stateManager.updateDisplayStatus(displayId, online);
+
+    switch(action) {
+      case "SEND_FAILURE_EMAIL":
+        return module.exports.sendFailureEmail(displayId, addresses);
+
+      case "SEND_RECOVERY_EMAIL":
+        return module.exports.sendRecoveryEmail(displayId, addresses);
+    }
+  });
+}
+
+function sendFailureEmail(displayId, addresses) {
+}
+
+function sendRecoveryEmail(displayId, addresses) {
 }
 
 module.exports = {
+  sendFailureEmail,
+  sendRecoveryEmail,
   updateDisplayStatusList
 };
