0a73eaa5eff43b933ca3c9db8a6035d30241f299
santiagonoguez <santiago@nul-unu.com>
2018-02-01 14:28:29 -0600

    implement main iteration

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const notifier = require("./src/notifier.js");
-const runner = require("./src/query-runner.js");
-const temporaryInterval = 60000;
+/* eslint-disable no-magic-numbers */
+
+const notifier = require("./src/notifier");
+const runner = require("./src/query-runner");
+const stateRetriever = require("./src/connection-state-retriever");
+
+const MINUTES = 60000;
+const monitoringInterval = 5 * MINUTES;
 
 require("assert")(notifier);
 require("assert")(runner);
 
-setInterval(temporaryHeartbeat, temporaryInterval);
+function generateStatusList(displays, onlineDisplayIds) {
+  return displays.map(display => {
+    const online = onlineDisplayIds.includes(display.displayId);
 
-function temporaryHeartbeat() {
-  console.log("I'm alive");
+    return Object.assign({online}, display);
+  });
 }
+
+function monitorDisplays() {
+  return runner.readMonitoredDisplays()
+  .then(displays => {
+    const displayIds = displays.map(display => display.displayId);
+
+    return stateRetriever.retrieveState(displayIds)
+    .then(onlineDisplayIds => {
+      const statusList = generateStatusList(displays, onlineDisplayIds);
+
+      return notifier.updateDisplayStatusListAndNotify(statusList);
+    });
+  })
+  .catch(console.error);
+}
+
+function run(schedule = setInterval) {
+  schedule(monitorDisplays, monitoringInterval);
+}
+
+module.exports = {generateStatusList, monitorDisplays, run};
