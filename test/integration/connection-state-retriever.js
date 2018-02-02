29e9d25a194bc93f1dcecd5b8cbddcbb2d5aa6b9
Tyler Johnson <mail@tyler-johnson.ca>
2018-02-02 16:55:40 -0500

    Call batch smembers and return connection state
    A call for state of ["A", "B", "C"]
    will return [1, 1, 0] if A and B are connected.

--- test/integration/connection-state-retriever.js
+++ test/integration/connection-state-retriever.js
@@ -21,17 +21,46 @@ describe("Connection State - Integration", ()=>{
   after(()=>redisClient.quit().then(retriever.quit));
 
   it("hasMockData", ()=>{
-    return redisClient.smembers("connected-displays")
+    return redisClient.smembers("connections:id")
     .then(resp=>{
-      console.dir(resp);
       assert.deepEqual(resp.sort(), initialConnectedDisplays.sort());
     });
+  });
+
+  it("retrieves connection state for one display", ()=>{
+    return retriever.retrieveState(["A"])
+    .then(resp=>{
+      assert.deepEqual(resp, [1]);
+    });
+  });
+
+  it("retrieves connection state for multiple displays", ()=>{
+    return retriever.retrieveState(["A", "B", "X"])
+    .then(resp=>{
+      assert.deepEqual(resp, [1, 1, 0]);
+    });
+  });
+
+  it("rejects if not array", ()=>{
+    return retriever.retrieveState({})
+    .then(()=>assert.fail("should have rejected"))
+    .catch(err=>{
+      if (err.message === "should have rejected") {return Promise.reject(err);}
+    })
+  });
+
+  it("rejects if empty array", ()=>{
+    return retriever.retrieveState([])
+    .then(()=>assert.fail("should have rejected"))
+    .catch(err=>{
+      if (err.message === "should have rejected") {return Promise.reject(err);}
+    })
   });
 });
 
 function setUpMockData() {
-  return redisClient.del("connected-displays")
+  return redisClient.del("connections:id")
   .then(()=>{
-    return redisClient.sadd("connected-displays", initialConnectedDisplays);
+    return redisClient.sadd("connections:id", ...initialConnectedDisplays);
   });
 }
